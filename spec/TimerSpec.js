71eb2b73d8ba10b2b2f921a48d06fa97785d7060
Anne Epstein <ajepst@gmail.com>
2012-07-04 23:21:53 -0500

    object-ify timer, cleanup, jasmine tests

---
+++ spec/TimerSpec.js
@@ -0,0 +1,314 @@
+//jasmine.getFixtures().fixturesPath = '/spec/HtmlFixture.html';.
+
+
+describe("Timer that is not started", function() {
+  var timer;
+
+  beforeEach(function() {
+    timer = new Timer();
+	loadFixtures('HtmlFixture.html');
+  });
+  
+  it("should have the full remaining time", function() {
+    expect(timer.remainingTime).toEqual(timer.endCount);
+  });  
+  
+  it("should display blank time", function() {
+    expect($('#countdownDisplay')).toHaveText('')
+  }); 
+  
+      it("should have KeepTalking", function() {
+    expect($("#PageContainer")).toHaveClass('KeepTalking');
+  });
+});
+
+describe("Timer that is a half second", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    jasmine.Clock.useMock();
+    timer = new Timer();
+	timer.startTimer();
+	jasmine.Clock.tick(500);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+ 
+  it("should have the full time left", function() {
+    expect(timer.remainingTime).toBe(timer.endCount);
+  });
+  
+    it("should display full time", function() {
+    expect($("#countdownDisplay")).toHaveText('5:00')
+  });
+  
+    it("should have KeepTalking", function() {
+    expect($("#PageContainer")).toHaveClass('KeepTalking');
+  });
+  
+});
+
+describe("Timer that is started one second", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    timer = new Timer();
+	jasmine.Clock.useMock();
+	timer.startTimer();
+	jasmine.Clock.tick(1200);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+ 
+  it("should have one second take off", function() {
+    expect(timer.remainingTime).toBe(timer.endCount - timer.second);
+  });
+  
+    it("should display time minus one second", function() {
+    expect($("#countdownDisplay")).toHaveText('4:59')
+  });
+  
+});
+
+describe("Timer that is 3:59", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    timer = new Timer();
+	jasmine.Clock.useMock();
+	timer.startTimer();
+	jasmine.Clock.tick(timer.minute*4-timer.second);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+  
+  it("should display 1:01", function() {
+    expect($("#countdownDisplay")).toHaveText('1:01');
+  });
+  
+  it("should have KeepTalking", function() {
+    expect($("#PageContainer")).toHaveClass('KeepTalking');
+  });
+  
+});
+
+
+describe("Timer that is 4:00", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    timer = new Timer();
+	jasmine.Clock.useMock();
+	timer.startTimer();
+	jasmine.Clock.tick(timer.minute*4);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+  
+  it("should display 1:00", function() {
+    expect($("#countdownDisplay")).toHaveText('1:00');
+  });
+  
+  it("should not have KeepTalking", function() {
+    expect($("#PageContainer")).not.toHaveClass('KeepTalking');
+  });
+  
+  it("should have OneMinuteLeft", function() {
+    expect($("#PageContainer")).toHaveClass('OneMinuteLeft');
+  });
+  
+});
+
+describe("Timer that is 0:11", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    timer = new Timer();
+	jasmine.Clock.useMock();
+	timer.startTimer();
+	jasmine.Clock.tick(timer.minute*4+timer.second*50-timer.second);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+  
+  it("should display 0:11", function() {
+    expect($("#countdownDisplay")).toHaveText('0:11');
+  });
+  
+  it("should not have KeepTalking", function() {
+    expect($("#PageContainer")).not.toHaveClass('KeepTalking');
+  });
+  
+  it("should have OneMinuteLeft", function() {
+    expect($("#PageContainer")).toHaveClass('OneMinuteLeft');
+  });
+  
+    it("should not have Warning", function() {
+    expect($("#PageContainer")).not.toHaveClass('Warning');
+  });
+ });
+ 
+describe("Timer that is 0:10", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    timer = new Timer();
+	jasmine.Clock.useMock();
+	timer.startTimer();
+	jasmine.Clock.tick(timer.minute*4+timer.second*50+300);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+  
+  it("should display 0:10", function() {
+    expect($("#countdownDisplay")).toHaveText('0:10');
+  });
+  
+  it("should not have KeepTalking", function() {
+    expect($("#PageContainer")).not.toHaveClass('KeepTalking');
+  });
+  
+  it("should have OneMinuteLeft", function() {
+    expect($("#PageContainer")).toHaveClass('OneMinuteLeft');
+  });
+  
+    it("should have Warning", function() {
+    expect($("#countdownDisplay")).toHaveClass('Warning');
+  });
+});
+
+describe("Timer that is 0:01", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    timer = new Timer();
+	jasmine.Clock.useMock();
+	timer.startTimer();
+	jasmine.Clock.tick(timer.minute*5-timer.second);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+  
+  it("should display 0:01", function() {
+    expect($("#countdownDisplay")).toHaveText('0:01');
+  });
+  
+  it("should not have KeepTalking", function() {
+    expect($("#PageContainer")).not.toHaveClass('KeepTalking');
+  });
+  
+  it("should have OneMinuteLeft", function() {
+    expect($("#PageContainer")).toHaveClass('OneMinuteLeft');
+  });
+  
+    it("should have Warning", function() {
+    expect($("#countdownDisplay")).toHaveClass('Warning');
+  });
+});
+
+describe("Timer that is 0:00", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    timer = new Timer();
+	jasmine.Clock.useMock();
+	timer.startTimer();
+	jasmine.Clock.tick(timer.minute*5);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+  
+  it("should display 0:00", function() {
+    expect($("#countdownDisplay")).toHaveText('0:00');
+  });
+  
+  it("should not have KeepTalking", function() {
+    expect($("#PageContainer")).not.toHaveClass('KeepTalking');
+  });
+  
+  it("should have TimeOut", function() {
+    expect($("#PageContainer")).toHaveClass('TimeOut');
+  });
+  
+    it("should not have Warning", function() {
+    expect($("#countdownDisplay")).not.toHaveClass('Warning');
+  });
+});
+
+describe("Timer that is -0:01", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    timer = new Timer();
+	jasmine.Clock.useMock();
+	timer.startTimer();
+	jasmine.Clock.tick(timer.minute*5 + timer.second);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+  
+  it("should display -0:01", function() {
+    expect($("#countdownDisplay")).toHaveText('-0:01');
+  });
+  
+  it("should not have KeepTalking", function() {
+    expect($("#PageContainer")).not.toHaveClass('KeepTalking');
+  });
+  
+  it("should have TimeOut", function() {
+    expect($("#PageContainer")).toHaveClass('TimeOut');
+  });
+  
+    it("should not have Warning", function() {
+    expect($("#countdownDisplay")).not.toHaveClass('Warning');
+  });
+});
+
+describe("Timer that is -1:00", function() {
+  var timer;
+  beforeEach(function() {
+    loadFixtures('HtmlFixture.html');
+    timer = new Timer();
+	jasmine.Clock.useMock();
+	timer.startTimer();
+	jasmine.Clock.tick(timer.minute*5 + timer.minute);
+  });
+  
+  afterEach(function() {
+    timer.clearTimerHooks();
+  });
+  
+  it("should display -1:00", function() {
+    expect($("#countdownDisplay")).toHaveText('-1:00');
+  });
+  
+  it("should not have KeepTalking", function() {
+    expect($("#PageContainer")).not.toHaveClass('KeepTalking');
+  });
+  
+  it("should have TimeOut", function() {
+    expect($("#PageContainer")).toHaveClass('TimeOut');
+  });
+  
+    it("should not have Warning", function() {
+    expect($("#countdownDisplay")).not.toHaveClass('Warning');
+  });
+});
---
+++ src/timer.js
@@ -0,0 +1,82 @@
+function Timer(totalMinutes) {
+this.totalMinutes = totalMinutes;
+
+this.second = 1000;
+this.minute = (this.second * 60);
+this.endCount = this.minute * this.totalMinutes;
+this.remainingTime = this.endCount;
+this.warningInterval;
+this.secondsInterval;
+this.warningTimeout;
+this.endTimeout;
+this.minuteTimeout;
+this.idee=Math.floor(Math.random()*10000)
+}
+
+Timer.prototype.displayTime = function () {
+  var timesUp, minutesLeft, msLeft, secondsLeft;
+  timesUp = false;
+  if (this.remainingTime < 0) { timesUp = true; }
+  minutesLeft = Math.floor(Math.abs(this.remainingTime) / this.minute);
+  msLeft = Math.abs(this.remainingTime) - (minutesLeft * this.minute);
+  secondsLeft = Math.floor(msLeft / this.second);
+  if (timesUp) {
+    minutesLeft = "-" + minutesLeft;
+  }
+  $("#countdownDisplay").text(minutesLeft + ":" + this.pad(secondsLeft, 2));
+};
+
+Timer.prototype.startTimer = function () {
+  var thisTimer = this;
+  this.clearTimerHooks();
+  this.secondsInterval = window.setInterval(function () { thisTimer.tick(); }, this.second);
+  this.warningTimeout =  window.setTimeout(function () {  thisTimer.setWarning(); }, this.remainingTime - this.second * 10 );
+  this.endTimeout = window.setTimeout(function () { thisTimer.setEndOfTime(); }, this.remainingTime );
+  this.minuteTimeout =  window.setTimeout(function () { thisTimer.setMinuteWarning(); }, this.remainingTime - this.minute );
+  this.displayTime();
+  };
+
+  Timer.prototype.setMinuteWarning = function () {
+	$("#PageContainer").removeClass("KeepTalking").addClass("OneMinuteLeft"); 
+  }
+  
+
+Timer.prototype.tick = function () {
+  this.remainingTime = this.remainingTime - this.second;
+  this.displayTime();
+};
+
+Timer.prototype.setWarning = function () {
+  var countdown = $("#countdownDisplay");
+  countdown.addClass("Warning");
+  this.warningInterval = window.setInterval(function () {countdown.toggle(); }, 250);
+};
+
+Timer.prototype.setEndOfTime = function () {
+  $("#PageContainer").removeClass("OneMinuteLeft").addClass("TimeOut");
+  window.clearInterval(this.warningInterval);
+  $("#countdownDisplay").removeClass("Warning").show();
+};
+
+Timer.prototype.pauseTimer = function () {
+  var thisTimer = this;
+  thisTimer.clearTimerHooks();
+  var countdown = $("#countdownDisplay");
+  countdown.show();
+};
+
+Timer.prototype.clearTimerHooks = function() {
+  window.clearInterval(this.secondsInterval);
+  window.clearTimeout(this.warningTimeout);
+  window.clearTimeout(this.endTimeout);
+  window.clearTimeout(this.minuteTimeout);
+  window.clearInterval(this.warningInterval);
+}
+
+Timer.prototype.pad = function (number, length) {
+  var str = number.toString();
+  while (str.length < length) {str = '0' + str; }
+  return str;
+};
+
+
